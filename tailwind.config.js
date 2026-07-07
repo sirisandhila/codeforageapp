c50717c83b9157058d9d5657b39a892aa4df4ab3
SiriSandhila <sirigoud05@gmail.com>
2026-07-07 12:18:06 +0530

    Update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-});
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
